4fe1f1d8f466988a0e737e11ab3ffe6e2a252cca
Denliner <35616129+hhamor@users.noreply.github.com>
2020-04-23 17:26:52 -0700

    Cloning from RocketPatrol

---
+++ src/main.js
@@ -0,0 +1,16 @@
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 480,
+    scene: [ Menu, Play ]
+}
+
+let keyF, keyLEFT, keyRIGHT;
+
+let game = new Phaser.Game(config);
+
+// define game settings
+game.settings = {
+    spaceshipSpeed: 3,
+    gameTimer: 60000    
+}
