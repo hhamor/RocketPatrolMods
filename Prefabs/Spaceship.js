f922867efddb4b2077fa1f948f21f644a7692653
Denliner <35616129+hhamor@users.noreply.github.com>
2020-05-11 13:45:20 -0700

    spaceship

--- Prefabs/Spaceship.js
+++ Prefabs/Spaceship.js
@@ -9,8 +9,35 @@ class Spaceship extends Phaser.GameObjects.Sprite {
     }
 
     update(){
+        // random movement 
+        let shipNum = Math.between(0,5);
+
+        switch(shipNum){
+            case 1:
+                this.x -= game.settings.spaceshipSpeed;
+                console.log("Case 1");
+                break;
+            case 2:
+                this.x += game.settings.spaceshipSpeed;
+                console.log("Case 2");
+                break;
+            case 3:
+                this.x -= (game.settings.spaceshipSpeed / 2);
+                console.log("Case 3");
+                break;
+            case 4:
+                this.x += (game.settings.spaceshipSpeed / 2);
+                console.log("Case 4");
+                break;
+            case 5:
+                this.x -= (game.settings.spaceshipSpeed * 2);
+                console.log("Case 5");
+                break;
+        }
+
+
         // move spaceship left
-        this.x -= game.settings.spaceshipSpeed;
+        //this.x -= game.settings.spaceshipSpeed;
 
         // wraparound from left to right edge 
         if(this.x <= 0-this.width){
