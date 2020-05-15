591e5057692c5b1c53d0a810e81edb5cb2d5a972
Denliner <35616129+hhamor@users.noreply.github.com>
2020-05-15 01:51:48 -0700

    tilemap

--- src/Scenes/Play.js
+++ src/Scenes/Play.js
@@ -5,13 +5,22 @@ class Play extends Phaser.Scene {
     
     // Note: Preload comes before create!
     preload(){
-        // load images/tile sprites
+        // Old Code
+        /*// load images/tile sprites
         this.load.image('rocket', './Assets/rocket.png');
         this.load.image('spaceship', './Assets/spaceship.png');
         this.load.image('starfield', './Assets/starfield.png');
 
         // load explosion spritesheet
-        this.load.spritesheet('explosion', './Assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
+        this.load.spritesheet('explosion', './Assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});*/
+
+        // Modded Code
+        // Background Grass Tilemap
+        this.load.image('tiles', './Assets/TilesetTemplate.png');
+        this.load.tilemapTiledJSON('map', './Assets/GrassBG.json');
+
+        // Top Level Frame
+        this.load.image('woodFrame', './Assets/WoodFrame.png');
     }
 
     // Note: Do not hard code dimensions in future games, calculate resolution instead!
@@ -22,17 +31,7 @@ class Play extends Phaser.Scene {
             frames: this.anims.generateFrameNumbers('explosion', { start: 0, end: 9, first: 0}),
             frameRate: 30
         });
-        // Note: Set tilesprites above static layers. Phaser renders objects in the order in which they are called, from lowest to highest.
-        // place tile sprite
-        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
 
-        // white rectangle borders
-        this.add.rectangle(5, 5, 630, 32, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(5, 443, 630, 32, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(5, 5, 32, 455, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(603, 5, 32, 455, 0xFFFFFF).setOrigin(0, 0);
-        // green UI background
-        this.add.rectangle(37, 42, 566, 64, 0x00FF00).setOrigin(0, 0);
 
         // rocket
         this.p1Rocket = new Rocket(this, game.config.width/2 - 8, 431, 'rocket').setScale(0.5, 0.5).setOrigin(0, 0);
@@ -41,6 +40,15 @@ class Play extends Phaser.Scene {
         this.ship01 = new Spaceship(this, game.config.width + 192, 132, 'spaceship', 0, 30).setOrigin(0,0);
         this.ship02 = new Spaceship(this, game.config.width + 96, 196, 'spaceship', 0, 20).setOrigin(0,0);
         this.ship03 = new Spaceship(this, game.config.width, 260, 'spaceship', 0, 10).setOrigin(0,0);
+
+
+        // wood frame
+        this.woodFrame = this.add.image(game.config.width/2, game.config.height/2, 'woodFrame');
+
+        // grass brackground
+        this.grassTilemap = this.add.tilemap('map');
+        this.grassImage = this.grassTilemap.addTilesetImage('TilesetTemplate', 'tiles');
+        this.grassBG = this.grassTilemap.createStaticLayer('Tile Layer 1', 'tilemap', 0, 0);
 
         // define keys
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
@@ -97,7 +105,7 @@ class Play extends Phaser.Scene {
         }
 
         // starfield tilesprite
-        this.starfield.tilePositionX -= 4;
+        //this.starfield.tilePositionX -= 4;
 
         // check for game over condition
         if(!this.gameOver){
